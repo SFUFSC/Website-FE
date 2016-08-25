95761cb0ced14eed5a71e4026cd7617173c65765
Dallan Bhatti <dbhatti@sfu.ca>
2016-08-25 22:34:17 +0000

    changes for heroku

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Workspace
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.action_mailer.default_url_options = { host: 'sheltered-anchorage-78577.herokuapp.com' }
+    config.action_mailer.default_url_options = { host: 'sfusports.herokuapp.com' }
   
     config.action_mailer.smtp_settings = {
       :address    => "smtp.gmail.com",
