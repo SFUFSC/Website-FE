cceb6f51e2f9af42213065cc6229275c4b0ad7d2
Dallan Bhatti <dbhatti@sfu.ca>
2016-08-25 23:22:39 +0000

    more heroku

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,10 @@ Rails.application.routes.draw do
 
   # get 'users/destroy'
 
+  
   # devise_for :users
   devise_for :users, controllers: { sessions: 'users/sessions', invitations: 'users/invitations', confimations: 'users/confimations', 
   mailer: 'users/mailer', passwords: 'users/passwords', registrations: 'users/registrations', unlocks: 'users/unlocks'}
-
 
   
   # The priority is based upon order of creation: first created -> highest priority.
